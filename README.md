eb44dae57e63847c5fc98ccf8ebfb14caee70125
k-stiles <michealessuman08@gmail.com>
2023-05-25 18:58:27 +0000

    added docker to run mysql

--- README.md
+++ README.md
@@ -1,8 +1,37 @@
 # Roaming_mavricks_tourism_app
+
 An HTML, CSS and java application for tourist
 
+## Dockerfile
+
+The project has a docker file in its root. To run the project with docker, do the following in the project terminal:
+
+- run command
+
+```
+    1. docker-compose up -d
+    2. docker exec -it db_tourism_container /bin/bash
+```
+
+- using mysql tourism_db:
+
+```
+    1. mysql -proot
+    2. use tourism_db;
+```
+
+- pull down docker processes:
+
+```
+     docker-compose down --rmi all -v
+```
+
+---
+
 ## Landing Page
+
 <img width="1440" alt="Home Page" src="https://github.com/K-stiles/Roaming_mavricks_tourism_app/assets/80682692/405a8f2d-758f-4c44-8253-9dca475d15d0">
 
 ### Summary
+
 "Our tourism application is a comprehensive and user-friendly platform designed to assist travelers in planning, organizing, and enhancing their travel experiences. With our app, users can explore a wide range of destinations, browse through detailed information about attractions, landmarks, and local services. They can also discover personalized recommendations, create itineraries, and book accommodations, transportation, and tours directly through the app. Our goal is to provide travelers with a seamless and convenient tool that enriches their journey, making it easier to discover new destinations and make the most out of their travel adventures."
--- db/tourism_db_dump_has_Insert_data.sql
+++ db/tourism_db_dump_has_Insert_data.sql
@@ -1,4 +1,4 @@
-CREATE DATABASE  IF NOT EXISTS `tourism_db` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
+CREATE DATABASE  IF NOT EXISTS `tourism_db` /*!40100 DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
 USE `tourism_db`;
 -- MySQL dump 10.13  Distrib 8.0.32, for Win64 (x86_64)
 --
@@ -23,7 +23,7 @@ USE `tourism_db`;
 
 DROP TABLE IF EXISTS `bookings`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
+/*!50503 SET character_set_client = utf8 */;
 CREATE TABLE `bookings` (
   `customer_id` int NOT NULL,
   `tour_id` int NOT NULL,
@@ -31,18 +31,17 @@ CREATE TABLE `bookings` (
   KEY `FK_TOURS_idx` (`tour_id`),
   CONSTRAINT `FK_CUSTOMERS` FOREIGN KEY (`customer_id`) REFERENCES `customers` (`customer_id`),
   CONSTRAINT `FK_TOURS` FOREIGN KEY (`tour_id`) REFERENCES `tours` (`tour_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
+
 
 --
 -- Dumping data for table `bookings`
 --
-
 LOCK TABLES `bookings` WRITE;
-/*!40000 ALTER TABLE `bookings` DISABLE KEYS */;
 INSERT INTO `bookings` VALUES (7,1),(1,2),(6,3);
-/*!40000 ALTER TABLE `bookings` ENABLE KEYS */;
 UNLOCK TABLES;
+
 
 --
 -- Table structure for table `customers`
@@ -50,7 +49,7 @@ UNLOCK TABLES;
 
 DROP TABLE IF EXISTS `customers`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
+/*!50503 SET character_set_client = utf8 */;
 CREATE TABLE `customers` (
   `customer_id` int NOT NULL AUTO_INCREMENT,
   `name` varchar(45) DEFAULT NULL,
@@ -58,18 +57,17 @@ CREATE TABLE `customers` (
   `email` varchar(45) DEFAULT NULL,
   `password` varchar(45) DEFAULT NULL,
   PRIMARY KEY (`customer_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
+
 
 --
 -- Dumping data for table `customers`
 --
-
 LOCK TABLES `customers` WRITE;
-/*!40000 ALTER TABLE `customers` DISABLE KEYS */;
 INSERT INTO `customers` VALUES (1,'jane doe','Adenta 8th avenue','jane@gmail.com','123456789'),(5,'james appiah','Kaneshi 121','j@gmail.com','wqfdstrgfj'),(6,'abraham lincoln','usa','ab@gmail.com','iamabraham'),(7,'mike','mccarthy hill','h@gmail.com','72r65214712');
-/*!40000 ALTER TABLE `customers` ENABLE KEYS */;
 UNLOCK TABLES;
+
 
 --
 -- Table structure for table `tour_guides`
@@ -77,25 +75,25 @@ UNLOCK TABLES;
 
 DROP TABLE IF EXISTS `tour_guides`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
+/*!50503 SET character_set_client = utf8 */;
 CREATE TABLE `tour_guides` (
   `tour_guide_id` int NOT NULL AUTO_INCREMENT,
   `name` varchar(45) DEFAULT NULL,
   `email` varchar(45) DEFAULT NULL,
   `password` varchar(45) DEFAULT NULL,
   PRIMARY KEY (`tour_guide_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
+
 
 --
 -- Dumping data for table `tour_guides`
 --
-
 LOCK TABLES `tour_guides` WRITE;
-/*!40000 ALTER TABLE `tour_guides` DISABLE KEYS */;
 INSERT INTO `tour_guides` VALUES (1,'philip gyan','phil@gmail.com','3235498707967');
-/*!40000 ALTER TABLE `tour_guides` ENABLE KEYS */;
 UNLOCK TABLES;
+
+
 
 --
 -- Table structure for table `tours`
@@ -103,7 +101,7 @@ UNLOCK TABLES;
 
 DROP TABLE IF EXISTS `tours`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
+/*!50503 SET character_set_client = utf8 */;
 CREATE TABLE `tours` (
   `tour_id` int NOT NULL AUTO_INCREMENT,
   `name` varchar(45) DEFAULT NULL,
@@ -114,18 +112,19 @@ CREATE TABLE `tours` (
   PRIMARY KEY (`tour_id`),
   KEY `FK_TOUR_GUIDES_idx` (`tour_guide_id`),
   CONSTRAINT `FK_TOUR_GUIDES` FOREIGN KEY (`tour_guide_id`) REFERENCES `tour_guides` (`tour_guide_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
+
 
 --
 -- Dumping data for table `tours`
 --
-
 LOCK TABLES `tours` WRITE;
-/*!40000 ALTER TABLE `tours` DISABLE KEYS */;
 INSERT INTO `tours` VALUES (1,'nzulazu','eastern','80',NULL,1),(2,'golden stool','asanti','464',NULL,1),(3,'elmina castle','central','64',NULL,1),(4,'something','north','678',NULL,1);
-/*!40000 ALTER TABLE `tours` ENABLE KEYS */;
 UNLOCK TABLES;
+
+
+
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
